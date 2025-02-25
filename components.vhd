f98decf3ee235260d908b7533ec609a1d9ba3221
R. Parthipan <rp732@cam.ac.uk>
2025-02-25 14:54:09 +0000

    Completed entity and component skeletons

---
+++ components.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity counter is 
+	port(clk: in std_logic;
+	slow_clk: out std_logic);
+end counter;
+	
+architecture behaviour of counter is
+	signal count : std_logic_vector(23 downto 0);
+	signal tick : std_logic;
+begin
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			count <= count + 1;
+			if count = 0 then -- overflow, takes about 0.67s with 50 MHz clock
+				tick<=not tick; -- change output from 0 to 1 or vice versa
+			end if;
+		end if;
+	end process;
+	slow_clk<=tick; -- one tick should be about 0.67s
+end behaviour;
--- morse_code_encoder.vhd
+++ morse_code_encoder.vhd
@@ -2,5 +2,16 @@ library IEEE;
 use ieee.std_logic_1164.all;
 
 entity morse_code_encoder is
-	port ();
+	port (clk : in std_logic;
+		sw : in std_logic_vector(0 to 3);
+		start: in std_logic;
+		reset: in std_logic;
+		led: out std_logic);
 end morse_code_encoder;
+
+architecture behaviour of morse_code_encoder is
+component counter
+	port (clk : in std_logic;
+	slow_clk: out std_logic);
+begin
+end behaviour;
